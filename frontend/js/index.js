8c6fa50b3900ec511f4b86ba254f4a397ade415c
Eric Chinen <jenkins@singu.com.br>
2018-11-06 18:01:17 -0200

    add components for html.

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,18 +1,46 @@
+var $ = jQuery;
 var socket = io();
 
 socket.on('connect', () => {
-    console.log('Connected to server!')
+    console.log('Conectando com o servidor!')
 });
 
-/* socket.on('disconnect', () => {
-    console.log('Disconnected from server!')
-}); */
-
-/* socket.emit('createMessage', {
-    from: 'Eric Front',
-    text: 'Hey this is a test from front.'
-}) */
+socket.on('disconnect', () => {
+    console.log('Desconectando com o servidor!')
+});
 
 socket.on('newMessage', (message) => {
-    console.log('newMessage', message)
+    console.log('Nova mensagem:', message)
+
+    var li = $('<li></li>');
+    li.text(`${message.from}: ${message.text}`)
+
+    $('#messages').append(li);
+});
+
+$('#message-form').on('submit', (e) => {
+    e.preventDefault();
+
+    socket.emit('createMessage', {
+        from: 'User',
+        text: $('[name=message]').val()
+    }, () => {
+
+    })
+});
+
+var locationButton = $('#send-location');
+locationButton.on('click', () => {
+    if (!navigator.geolocation) {
+        return alert('Geolocalização não é suportada pelo seu browser.')
+    }
+
+    navigator.geolocation.getCurrentPosition((position) => {
+        socket.emit('createLocationMessage', {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        });
+    }, () => {
+        alert('Não foi possível buscar o local.')
+    })
 })
