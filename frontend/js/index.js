b446d5ec38cf17c2e2e1e960c1b88775d7bac08a
Eric Chinen <jenkins@singu.com.br>
2018-11-07 02:23:35 -0200

    add css style.

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,5 +1,5 @@
-var $ = jQuery;
-var socket = io();
+const $ = jQuery;
+const socket = io();
 
 socket.on('connect', () => {
     console.log('Conectando com o servidor!')
@@ -12,15 +12,15 @@ socket.on('disconnect', () => {
 socket.on('newMessage', (message) => {
     console.log('Nova mensagem:', message)
 
-    var li = $('<li></li>');
+    const li = $('<li></li>');
     li.text(`${message.from}: ${message.text}`)
 
     $('#messages').append(li);
 });
 
 socket.on('newLocationMessage', (message) => {
-var li = $('<li></li>');
-var a = $('<a target="_blank">Minha atual localização</a>');
+const li = $('<li></li>');
+const a = $('<a target="_blank">Minha atual localização</a>');
 
 li.text(`${message.from}: `);
 a.attr('href', message.url);
@@ -31,26 +31,34 @@ $('#messages').append(li);
 $('#message-form').on('submit', (e) => {
     e.preventDefault();
 
+    const messageTextBox = $('[name=message]');
+
     socket.emit('createMessage', {
         from: 'User',
-        text: $('[name=message]').val()
+        text: messageTextBox.val()
     }, () => {
-
+        messageTextBox.val('')
     })
 });
 
-var locationButton = $('#send-location');
+const locationButton = $('#send-location');
 locationButton.on('click', () => {
     if (!navigator.geolocation) {
         return alert('Geolocalização não é suportada pelo seu browser.')
     }
 
+    locationButton.attr('disabled', 'disabled').text('Enviando localização...');
+
     navigator.geolocation.getCurrentPosition((position) => {
+        locationButton.removeAttr('disabled').text('Enviando localização');
+
         socket.emit('createLocationMessage', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
         });
     }, () => {
+        locationButton.removeAttr('disabled').text('Enviando localização');
+
         alert('Não foi possível buscar o local.')
     })
 })
