8107934a2e34fbb96d3e3a698540b9f28bc716dd
Eric Chinen <jenkins@singu.com.br>
2018-11-11 03:19:19 -0200

    fix message unit test.

--- backend/utils/message.test.js
+++ backend/utils/message.test.js
@@ -1,26 +1,29 @@
 const expect = require('expect');
+const moment = require('moment');
 const { generateMessage, generateLocationMessage } = require('./message');
 
 describe('Gerar mensagem', () => {
     it('Mostrar um objecto de mensagem com sucesso.', () => {
         const from = 'Eric';
         const text = 'Mensagem teste';
+        const createdAt = moment().valueOf();
+        
         const message = generateMessage(from, text);
 
-        expect(message.createdAt).toBeA('number');
-        expect(message).toInclude({ from, text });
+        expect(message).toEqual({ from, text, createdAt });
     });
 });
 
 describe('Gerar mensagem com localização', () => {
     it('Mostrar a localização do usuário com sucesso.', () => {
-        var from = 'User'
-        var latitude = 15;
-        var longitude = 19;
-        var url = 'https://www.google.com/maps?q=15,19';
+        const from = 'User'
+        const latitude = 15;
+        const longitude = 19;
+        const url = 'https://www.google.com/maps?q=15,19';
+        const createdAt = moment().valueOf();
+
         var message = generateLocationMessage(from, latitude, longitude);
 
-        expect(message.createdAt).toBeA('number');
-        expect(message).toInclude({ from, url });
+        expect(message).toEqual({ from, url, createdAt });
     });
 });
